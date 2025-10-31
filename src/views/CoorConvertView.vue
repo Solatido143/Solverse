93209087b3de4dc2eef610f95fb7da8d4b5e1a89
Mark Anthony Sol Cruz <solcruzmarkanthony@gmail.com>
2025-10-31 11:28:34 +0800

    Coordinate converter

--- src/views/CoorConvertView.vue
+++ src/views/CoorConvertView.vue
@@ -4,6 +4,9 @@
 
 <template>
     <main class="flex-grow p-2">
-
+        <label class="block !mb-2 text-sm font-medium text-raisin-black dark:text-floral-white">
+            Enter your coordinates <span class="text-accent-soft-ethereal">x, y, z</span>
+        </label>
+        <input class="text-raisin-black dark:text-floral-white">
     </main>
 </template>
